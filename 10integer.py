18bb48a1afc3b439ca1564843e41e18d30c96520
sunshay <sunshayina@outlook.com>
2022-06-20 18:52:09 -0700

    while

---
+++ 10integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# print the first 10 positif integer 
+a = 1
+while a < 10:
+    print(a)
+    a += 1
+
+
+for i in range(25,30):
+    print(i) 
---
+++ while.py
@@ -0,0 +1,9 @@
+from ast import While
+
+
+#!/usr/bin/python
+
+a = 1
+while a < 10:
+    print(a)
+    a += 1
